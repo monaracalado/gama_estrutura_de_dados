f9041331e5ac3b9bf310f84719aa7d9ef0a91ae7
Monara Calado <monaracalado@gmail.com>
2021-08-02 23:25:16 -0300

    Exemplo de busca Sequencia e Binaria

---
+++ busca.js
@@ -0,0 +1,50 @@
+//  criando uma ferramenta de busca sequencial
+var valores = [5, 8, 10, 22, 38, 45, 60, 90, 100, 120];
+function busca(num){
+    for(i = 0; i < 6; i++){
+        if (num == valores[i]){
+            return i;
+        }
+    }
+    return -1
+}
+
+
+// ultilizando a ferramenta de busca sequencial
+
+// solicitando um valor existente
+console.log(busca(10));
+
+// solicitando um valor invalido
+console.log(busca(50));
+
+
+
+// criando uma ferramenta de busca binaria 
+function buscaBin(num){
+    let inicio, fim, meio;
+    let passos= 0;
+    inicio = 0;
+    fim = 9;
+    while (inicio <= fim){
+        meio = parseInt((inicio + fim) / 2);
+        passos = passos + 1;
+        if (num == valores[meio]){
+            console.log("\nAchei em "+passos+" passos!")
+            return meio;
+        }else{
+            if (num > valores[meio]){
+                inicio = meio + 1;
+            }else{
+                fim = meio -1;
+            }
+        }
+    }
+    console.log("\nNão achei em " +passos+" passos!")
+    return -1;
+}
+
+// ultilizando a ferramenta de busca binaria 
+console.log(buscaBin(10));
+console.log(buscaBin(60));
+console.log(buscaBin(50));
